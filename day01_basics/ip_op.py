c905729c5a18c75f3727113a17adc9955932a6ef
Gauri Karale <gskarale2006@gmail.com>
2025-12-21 14:01:36 +0530

    edited ip_op

---
+++ day01_basics/ip_op.py
@@ -0,0 +1,7 @@
+num=int(input("Enter a number: "))
+num_dec=float(input("Enter a float value: "))
+print(f"{num} is an "+str(type(num)))
+print(f"{num_dec} is an "+str(type(num_dec)))
+
+print(len("12345"))
+print("Number of letters in your name: " + str(len(input('Enter your name'))))
